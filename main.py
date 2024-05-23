c9e94decc37f673604f1d536935ce45999084f20
Lilibeth <lilibethocando@gmail.com>
2024-05-22 23:42:37 -0500

    Add functionality

--- main.py
+++ main.py
@@ -1,32 +1,32 @@
 from flask import Flask, render_template
 import requests
-from post import Post
+
 
 app = Flask(__name__)
 
-blog_url = "https://api.npoint.io/c790b4d5cab58020d391"
+blog_url = "https://api.npoint.io/39cc79459d330a231db3"
 
-blog_objects = []
 response = requests.get(blog_url)
-data = response.json()
-
-for post in data:
-    post_obj = Post(post["id"], post["title"], post["subtitle"], post["body"])
-    blog_objects.append(post_obj)
-
+posts = response.json()
 
 
 @app.route('/')
-def home():
-    return render_template("index.html", all_posts=blog_objects)
+def get_all_posts():
+    return render_template("index.html", all_posts=posts)
 
-@app.route('/post/<int:index>')
-def show_post(index):
-    blog_post_id = None
-    for blog_post in blog_objects:
-        if blog_post.post_id == index:
-            blog_post_id = blog_post
-    return render_template("post.html", post=blog_post_id)
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
